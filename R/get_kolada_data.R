339d9784f26703e6d7e8f2e5c7b039e9330bff02
shwva184 <vcshwetha2@gmail.com>
2020-10-01 13:11:02 +0200

    Adding test cases

--- R/get_kolada_data.R
+++ R/get_kolada_data.R
@@ -53,11 +53,11 @@ getMetadata=function(entity,title=NULL)
 #'
 #' @return Returns a data.frame containing the data for a given kpi, municipality and year
 #'
-#' @examples fetch_with_all_given_entity("N00945",1860,c(2009,2007,2008))
+#' @examples fetch_all_given_entity("N00945",1860,c(2009,2007,2008))
 #' @export
 
-fetch_with_all_given_entity= function(kpi,municipality_id,year){
-  stopifnot(is.atomic(kpi),is.atomic(municipality_id))
+fetch_all_given_entity= function(kpi,municipality_id,year){
+  stopifnot(is.atomic(kpi),is.atomic(municipality_id),is.character(kpi),is.numeric(municipality_id))
   final_result=data.frame()
   for (i in year){
     url = paste("http://api.kolada.se/v2/data/kpi",kpi,"municipality",municipality_id,"year",i,sep= "/")
@@ -78,10 +78,11 @@ fetch_with_all_given_entity= function(kpi,municipality_id,year){
 #' @import tidyr
 #' @return Returns a data.frame containing the data for a given kpi and year
 #'
-#' @examples fetch_with_given_kpiandyear("N00905",2009)
+#' @examples fetch_given_kpiandyear("N00905",2009)
 #' @export
 #' 
-fetch_with_given_kpiandyear = function(kpi,year){
+fetch_given_kpiandyear = function(kpi,year){
+  stopifnot(is.character(kpi), is.vector(year),is.atomic(kpi))
   final_result=data.frame()
   for (i in year){
     url = paste("http://api.kolada.se/v2/data/kpi",kpi,"year",i,sep= "/")
@@ -103,10 +104,11 @@ fetch_with_given_kpiandyear = function(kpi,year){
 #' @import tidyr
 #' @return Returns a data.frame containing the data for a given municipality and year
 #'
-#' @examples fetch_with_given_muncipalityandyear(1860,2009)
+#' @examples fetch_given_muncipalityandyear(1860,2009)
 #' @export
 #' 
-fetch_with_given_muncipalityandyear = function(municipality_id,year){
+fetch_given_muncipalityandyear = function(municipality_id,year){
+  stopifnot(is.numeric(municipality_id), is.vector(year),is.atomic(municipality_id))
   final_result=data.frame()
   for (i in year){
     url = paste("http://api.kolada.se/v2/data/municipality",municipality_id,"year",i,sep= "/")
@@ -128,11 +130,12 @@ fetch_with_given_muncipalityandyear = function(municipality_id,year){
 #' @import tidyr
 #' @return Returns a data.frame containing the data for a given kpi and municipality.
 #'
-#' @examples fetch_with_given_kpiandmuncipality_id("N00945",1860)
+#' @examples fetch_given_kpiandmuncipality_id("N00945",1860)
 #' @export
 #' 
 
-fetch_with_given_kpiandmuncipality_id = function(kpi,municipality_id){
+fetch_given_kpiandmuncipality_id = function(kpi,municipality_id){
+  stopifnot(is.numeric(municipality_id), is.character(kpi),is.atomic(kpi),is.atomic(municipality_id))
   final_result=data.frame()
     url = paste("http://api.kolada.se/v2/data/kpi",kpi,"municipality",municipality_id,sep= "/")
     m1= GET(url)
--- tests/testthat/test_get_kolada_data.R
+++ tests/testthat/test_get_kolada_data.R
@@ -22,3 +22,49 @@ test_that("getMetadata() is working", {
   expect_equal(df[1, "member_id"], "1263")
   expect_equal(df[1, "member_title"], "Svedala")
 })
+
+test_that("fetch_all_given_entity() works",{
+  output=fetch_all_given_entity("N00945",1860,c(2009,2007,2008))
+  expect_equal(output[[5]][1],"T")
+  expect_equal(round(output[[7]][1]),39)
+  expect_true(is.data.frame(output))
+})
+
+test_that("fetch_given_kpiandyear() works",{
+  output=fetch_given_kpiandyear("N00905",2009)
+  expect_equal(output[[5]][5],"M")
+  expect_equal(round(output[[7]][1]),190730)
+  expect_true(is.data.frame(output))
+})
+
+test_that("fetch_given_muncipalityandyear() works",{
+  output=fetch_given_muncipalityandyear(1860,2009)
+  expect_equal(output[[5]][2],"T")
+  expect_equal(round(output[[7]][1]),55)
+  expect_true(is.data.frame(output))
+})
+
+test_that("fetch_given_kpiandmuncipality_id() works",{
+  output=fetch_given_kpiandmuncipality_id("N00945",1860)
+  expect_equal(output[[5]][2],"T")
+  expect_equal(output[[3]][4],1999)
+  expect_true(is.data.frame(output))
+})
+
+test_that("fetch_given_kpiandmuncipality_id() rejects errounous input",{
+  expect_error(fetch_given_kpiandmuncipality_id(45,1860))
+  expect_error(fetch_given_kpiandmuncipality_id("N00945","Ale"))
+  })
+
+test_that("fetch_given_muncipalityandyear() rejects errounous input",{
+  expect_error(fetch_given_muncipalityandyear("Ale",2009))
+})
+
+test_that("fetch_given_kpiandyear() rejects errounous input",{
+  expect_error(fetch_given_kpiandyear(45,2009))
+})
+
+test_that("fetch_all_given_entity() rejects errounous input",{
+  expect_error(fetch_all_given_entity(945,1860,2009))
+  expect_error((fetch_all_given_entity("N00945","Ale",2009)))
+})
